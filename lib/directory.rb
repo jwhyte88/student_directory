4fd14431f7e8b7e1fbfeedc960aed2239c633d47
jwhyte88 <jwhyte@opentable.com>
2014-06-05 13:15:11 +0100

    directory file set up, basic layout set up of how it should show in the command line.

---
+++ lib/directory.rb
@@ -0,0 +1,13 @@
+student_count = 4
+
+# First we print the list of students
+puts "The Students of my cohort at Makers Academy"
+puts "------------"
+puts "Hannibal Lecter"
+puts "Homer Simpson"
+puts "Phil McHunt"
+puts "Krusty the Klown"
+print "Overall, we have #{student_count}"
+# finally we prrint the total
+# it's important that print() doesn't add new line characters
+puts " great students"
